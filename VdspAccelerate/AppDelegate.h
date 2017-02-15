1ef87f8a7e35be20c120cb3d9e3fa0f2923c56dc
xiaokai.zhan <zhanxiaokai@changba.com>
2017-02-15 10:51:58 +0800

    Initial Commit

---
+++ VdspAccelerate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VdspAccelerate
+//
+//  Created by apple on 2017/2/15.
+//  Copyright © 2017年 xiaokai.zhan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
